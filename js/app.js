95bdd7ed01451e43c54067a774bf6a03aa91e5ad
elenaJO <ele.lucero1993@gmail.com>
2018-02-07 12:41:23 -0500

    prueba collage

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    // manda un alert si se ingresa un numero
+    $('#name').on('input', function() {
+      if ($(this).val().match(/[1-9]/)) {
+        alert('No ingrese números');
+        $(this).val('');
+      } 
+    });
+    
+    // valida si el correo es valido cuando ya no esta en focus
+    // $('#email').focusout(function() {
+    //   var PATTERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    //   var res = PATTERNEMAIL.test($(this).val());
+    //   if (res === false) {
+    //     alert('Ingrese correo válido');
+    //     $(this).val('');
+    //   }
+    // });
+  
+    // // el boton se habilita si todos los campos estan llenos y redirecciona a la vista end
+    // $('#submit').click(function(event) {
+    //   if ($('#check').prop('checked') && $('#first-name').val() && $('#last-name').val() && $('#email').val()) {
+    //     event.preventDefault();
+    //     window.location.href = '../views/end.html';
+    //   } else {
+    //     alert('Rellene todos los campos');
+    //     event.preventDefault();
+    //   }
+    // });
+  });
